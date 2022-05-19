6e2d88e985b1f3ec4143e94785c7047a35c90a58
Cozyamy <cozyamy@gmail.com>
2022-05-19 15:27:27 -0700

    correcting the simple shell

--- split.c
+++ split.c
@@ -1,110 +1,212 @@
-/*
- * File: split.c
- * Authors: Ihejiako Amarachi
- *          Obumneme Okoye
- */
-
-#include "shell.h"
-
-int token_len(char *str, char *delim);
-int count_tokens(char *str, char *delim);
-char **_strtok(char *line, char *delim);
+#include "main.h"
 
 /**
- * token_len - Locates the delimiter index marking the end
- *             of the first token contained within a string.
- * @str: The string to be searched.
- * @delim: The delimiter character.
+ * swap_char - swaps | and & for non-printed chars
  *
- * Return: The delimiter index marking the end of
- *         the intitial token pointed to be str.
+ * @input: input string
+ * @bool: type of swap
+ * Return: swapped string
  */
-int token_len(char *str, char *delim)
+char *swap_char(char *input, int bool)
 {
-	int index = 0, len = 0;
+	int i;
 
-	while (*(str + index) && *(str + index) != *delim)
+	if (bool == 0)
 	{
-		len++;
-		index++;
-	}
+		for (i = 0; input[i]; i++)
+		{
+			if (input[i] == '|')
+			{
+				if (input[i + 1] != '|')
+					input[i] = 16;
+				else
+					i++;
+			}
 
-	return (len);
+			if (input[i] == '&')
+			{
+				if (input[i + 1] != '&')
+					input[i] = 12;
+				else
+					i++;
+			}
+		}
+	}
+	else
+	{
+		for (i = 0; input[i]; i++)
+		{
+			input[i] = (input[i] == 16 ? '|' : input[i]);
+			input[i] = (input[i] == 12 ? '&' : input[i]);
+		}
+	}
+	return (input);
 }
 
 /**
- * count_tokens - Counts the number of delimited
- *                words contained within a string.
- * @str: The string to be searched.
- * @delim: The delimiter character.
+ * add_nodes - add separators and command lines in the lists
  *
- * Return: The number of words contained within str.
+ * @head_s: head of separator list
+ * @head_l: head of command lines list
+ * @input: input string
+ * Return: no return
  */
-int count_tokens(char *str, char *delim)
+void add_nodes(sep_list **head_s, line_list **head_l, char *input)
 {
-	int index, tokens = 0, len = 0;
+	int i;
+	char *line;
 
-	for (index = 0; *(str + index); index++)
-		len++;
+	input = swap_char(input, 0);
 
-	for (index = 0; index < len; index++)
+	for (i = 0; input[i]; i++)
 	{
-		if (*(str + index) != *delim)
+		if (input[i] == ';')
+			add_sep_node_end(head_s, input[i]);
+
+		if (input[i] == '|' || input[i] == '&')
 		{
-			tokens++;
-			index += token_len(str + index, delim);
+			add_sep_node_end(head_s, input[i]);
+			i++;
 		}
+	}
+
+	line = _strtok(input, ";|&");
+	do {
+		line = swap_char(line, 1);
+		add_line_node_end(head_l, line);
+		line = _strtok(NULL, ";|&");
+	} while (line != NULL);
+
+}
+
+/**
+ * go_next - go to the next command line stored
+ *
+ * @list_s: separator list
+ * @list_l: command line list
+ * @datash: data structure
+ * Return: no return
+ */
+void go_next(sep_list **list_s, line_list **list_l, data_shell *datash)
+{
+	int loop_sep;
+	sep_list *ls_s;
+	line_list *ls_l;
+
+	loop_sep = 1;
+	ls_s = *list_s;
+	ls_l = *list_l;
+
+	while (ls_s != NULL && loop_sep)
+	{
+		if (datash->status == 0)
+		{
+			if (ls_s->separator == '&' || ls_s->separator == ';')
+				loop_sep = 0;
+			if (ls_s->separator == '|')
+				ls_l = ls_l->next, ls_s = ls_s->next;
+		}
+		else
+		{
+			if (ls_s->separator == '|' || ls_s->separator == ';')
+				loop_sep = 0;
+			if (ls_s->separator == '&')
+				ls_l = ls_l->next, ls_s = ls_s->next;
+		}
+		if (ls_s != NULL && !loop_sep)
+			ls_s = ls_s->next;
+	}
+
+	*list_s = ls_s;
+	*list_l = ls_l;
+}
+
+/**
+ * split_commands - splits command lines according to
+ * the separators ;, | and &, and executes them
+ *
+ * @datash: data structure
+ * @input: input string
+ * Return: 0 to exit, 1 to continue
+ */
+int split_commands(data_shell *datash, char *input)
+{
+
+	sep_list *head_s, *list_s;
+	line_list *head_l, *list_l;
+	int loop;
+
+	head_s = NULL;
+	head_l = NULL;
+
+	add_nodes(&head_s, &head_l, input);
+
+	list_s = head_s;
+	list_l = head_l;
+
+	while (list_l != NULL)
+	{
+		datash->input = list_l->line;
+		datash->args = split_line(datash->input);
+		loop = exec_line(datash);
+		free(datash->args);
+
+		if (loop == 0)
+			break;
+
+		go_next(&list_s, &list_l, datash);
+
+		if (list_l != NULL)
+			list_l = list_l->next;
+	}
+
+	free_sep_list(&head_s);
+	free_line_list(&head_l);
+
+	if (loop == 0)
+		return (0);
+	return (1);
+}
+
+/**
+ * split_line - tokenizes the input string
+ *
+ * @input: input string.
+ * Return: string splitted.
+ */
+char **split_line(char *input)
+{
+	size_t bsize;
+	size_t i;
+	char **tokens;
+	char *token;
+
+	bsize = TOK_BUFSIZE;
+	tokens = malloc(sizeof(char *) * (bsize));
+	if (tokens == NULL)
+	{
+		write(STDERR_FILENO, ": allocation error\n", 18);
+		exit(EXIT_FAILURE);
+	}
+
+	token = _strtok(input, TOK_DELIM);
+	tokens[0] = token;
+
+	for (i = 1; token != NULL; i++)
+	{
+		if (i == bsize)
+		{
+			bsize += TOK_BUFSIZE;
+			tokens = _reallocdp(tokens, i, sizeof(char *) * bsize);
+			if (tokens == NULL)
+			{
+				write(STDERR_FILENO, ": allocation error\n", 18);
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = _strtok(NULL, TOK_DELIM);
+		tokens[i] = token;
 	}
 
 	return (tokens);
-}
-
-/**
- * _strtok - Tokenizes a string.
- * @line: The string.
- * @delim: The delimiter character to tokenize the string by.
- *
- * Return: A pointer to an array containing the tokenized words.
- */
-char **_strtok(char *line, char *delim)
-{
-	char **ptr;
-	int index = 0, tokens, t, letters, l;
-
-	tokens = count_tokens(line, delim);
-	if (tokens == 0)
-		return (NULL);
-
-	ptr = malloc(sizeof(char *) * (tokens + 2));
-	if (!ptr)
-		return (NULL);
-
-	for (t = 0; t < tokens; t++)
-	{
-		while (line[index] == *delim)
-			index++;
-
-		letters = token_len(line + index, delim);
-
-		ptr[t] = malloc(sizeof(char) * (letters + 1));
-		if (!ptr[t])
-		{
-			for (index -= 1; index >= 0; index--)
-				free(ptr[index]);
-			free(ptr);
-			return (NULL);
-		}
-
-		for (l = 0; l < letters; l++)
-		{
-			ptr[t][l] = line[index];
-			index++;
-		}
-
-		ptr[t][l] = '\0';
-	}
-	ptr[t] = NULL;
-	ptr[t + 1] = NULL;
-
-	return (ptr);
 }
